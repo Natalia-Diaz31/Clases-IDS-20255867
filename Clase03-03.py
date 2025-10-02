9a5a039525e52b54511903b4f9133311ef083a7f
Natalia Jimena Diaz <nataliadiazzz31@gmail.com>
2025-10-02 13:15:43 -0600

    Semana 3

---
+++ Clase03-03.py
@@ -0,0 +1,29 @@
+year = 2025
+period = 5 
+
+grad = year + period 
+nineth = year - 3
+salary = year * 0.25
+test = year/3 
+
+compa1 = year > nineth #Creo yo que si 
+compa2 = year < nineth #Creo yo que no
+compa3 = compa1 + 10
+compa4 = compa2 + 10
+
+""""
+print (grad)
+print (nineth)
+print (salary)      
+print (test)
+print (compa1)
+print (compa2)
+print (compa3)
+print (compa4) 
+"""
+
+
+pi= 3.14159
+print(pi)
+pi_int = int(pi)
+print(pi_int)
---
+++ Semana3/mi_primer_scrip.py
@@ -0,0 +1,10 @@
+#Este programa fue hecho por Python 3.13.7
+var1 = 100
+var2 = 200
+var3 = var1 + var2
+print ("Hello World")
+print(var3) 
+""""Este tipo de comilas se va a utilizar para hacer comentarios de varias lineas
+y no solo de una linea como el comentario de arriba"""
+
+
