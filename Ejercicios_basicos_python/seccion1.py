d849a5b6daa0249a0c8d0c34eccc8e55d69ec3f7
Natalia Jimena Diaz <nataliadiazzz31@gmail.com>
2025-10-07 16:15:46 -0600

    SECCIÓN 1: Variables, comentarios y print()

---
+++ Ejercicios_basicos_python/seccion1.py
@@ -0,0 +1,22 @@
+Name = "Natalia"
+#print(Name)
+
+Edad = 20 
+Ciudad = "Santa Tecla"
+Profesion = "Estudiante"
+
+print(f"hola mi nombre es {Name}, tengo {Edad} años, soy de {Ciudad} y soy {Profesion}")
+# Una variable sirve para guardar un valor, y ese valor puede cambiar a lo largo del programa.
+
+Mensaje = "Bienvenido al curso de Python"
+print(Mensaje)
+
+anio_actual = 2025 
+anio_nacimiento = 2005 
+Edad = anio_actual - anio_nacimiento
+print (Edad)
+
+Pi = 3.14159265359
+print (f"El valor de Pi es {Pi:.4f}") #el .2f me permite limitar a 4 decimales
+
+#La diferencia entre una variable de tipo entero y una variable de tipo cadena es que la variable de tipo entero solo puede contener números enteros, mientras que la variable de tipo cadena puede contener cualquier tipo de carácter, incluyendo letras, números y símbolos.
