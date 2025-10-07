a6e9823874d19844bb02e206122565585a51217f
Natalia Jimena Diaz <nataliadiazzz31@gmail.com>
2025-10-07 15:25:52 -0600

    clase 3 semana 4

---
+++ Semana4/clase04-03.py
@@ -0,0 +1,20 @@
+my_string = "ABCDEFGHI" #sring , cadena de caracteres 
+""" ItemIngresar  = input("Ingrese el item : ")
+print(f"el item ingresado es {ItemIngresar}") 
+letra = int(input("Indique el indice de la leta a mostrar (del 0 al 4) : ")) 
+#print(len(my_string))  """
+#print(len(my_string))  
+
+#print ("O" in my_string)
+#print ("AP"*8) # que el caracter que esta dentro se repita 8 veces 
+
+#print(min(my_string)) # muestra el caracter con menor valor de la cadena
+#print(max(my_string)) # muestra el caracter con mayor valor 
+
+#print(my_string.count"C"))# muestra cuantas veces se repite la letra C en la cadena
+
+
+
+
+
+ 
